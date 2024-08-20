bfc2029f21de4c228d85bf61f6642d9b67140149
chrischan <ziqichen55555@gmail.com>
2024-08-20 23:17:41 +0800

    fixing the bug

--- ui/my-app/app/results/page.tsx
+++ ui/my-app/app/results/page.tsx
@@ -1,4 +1,5 @@
 // app/results/page.tsx
+"use client";
 import React from 'react';
 import Header from '../components/Header';
 import ResultsForm from './ResultItem';
--- ui/my-app/app/search/SearchForm.tsx
+++ ui/my-app/app/search/SearchForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useRouter } from "next/router";
-import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import React, { useEffect,useState } from "react";
 
 const SearchForm = () => {
   const [query, setQuery] = useState("");
