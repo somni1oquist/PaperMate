ab37c7a4d640354a9e187d1bb71da329500bfb03
Lynn Huang <somniloquist.h@gmail.com>
2024-08-23 19:44:11 +0800

    Add logging setup
    Switch to disk-based db
    Remove unneeded files and imports

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,11 +1,12 @@
+from datetime import datetime
 import logging
+import os
 from flask import Flask
 from flask_migrate import Migrate, upgrade
 from flask_restx import Api
 from config import Config
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-import random
 
 db = SQLAlchemy()
 api = Api()
@@ -31,35 +32,39 @@ def create_app(config=None):
 
     from app.models.paper import Paper
 
-    # Set up logging
-    logging.basicConfig(filename='app.log', level=logging.INFO,
-                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # Logging setup
+    logger = init_logging()
+    
+    # Log application start
+    logger.info(f'{api.title} started')
 
-    logger = logging.getLogger(__name__)
-    logger.info('Starting the PaperMate API')
-
-    with app.app_context():
-        # Apply migrations
-        upgrade()
-
-        # Return error messages if any Errors occur
-        @app.errorhandler(Exception)
-        def handle_error(error):
-            logger.error(f'An error occurred: {type(error).__name__} - {str(error)}', exc_info=True)
-            if app.config['DEBUG']:
-                raise error
-            return {'message': str(error)}, 500
-
-        ''' Testing for logging function
-        @app.route('/trigger-error')
-        def trigger_error():
-            try:
-                raise ValueError("This is a test error")
-            except Exception as e:
-                logger.error(f'Error triggered in /trigger-error: {e}', exc_info=True)
-                raise
-        '''
+    # Return error messages if any Errors occur
+    @app.errorhandler(Exception)
+    def handle_error(error):
+        # Log in the format: ErrorType: Message
+        logger.error(f'{type(error).__name__}: {str(error)}')
+        if app.debug:
+            raise error
+        return {'message': str(error)}, 500
 
     return app
 
+def init_logging():
+    # Logging setup
+    logs_dir = 'logs'
+    if not os.path.exists(logs_dir):
+        os.makedirs(logs_dir)
+        
+    # File handler for application logs for current date
+    today = datetime.now().strftime('%Y-%m-%d')
+    file_handler = logging.FileHandler(f'{logs_dir}/{today}.log')
+    file_handler.setLevel(logging.INFO)
+    file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(file_formatter)
 
+    # Configure root logger
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(file_handler)
+
+    return logger
